3425b86da581243e733bc655159d1fbb3581b1ad
jakubbadacz <b00080699@student.itb.ie>
2017-10-04 16:52:26 +0100

    Added position while moving

--- Assets/Components/C2_navigation/Scripts/NavController.cs
+++ Assets/Components/C2_navigation/Scripts/NavController.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class NavController : MonoBehaviour
 {
+
     private NavModel navModel;
 
     void Awake()
     {
         navModel = new NavModel();
     }
+
 
     public NavModel GetNavModel()
     {
@@ -20,6 +23,8 @@ public class NavController : MonoBehaviour
         float y = GameObject.Find("Ship").transform.position.y;
 
         GameObject.Find("Ship").transform.position = new Vector3(x, y+10);
+
+        navModel.SetPositionY(y);
     }
 
     public void Backwards()
@@ -28,6 +33,8 @@ public class NavController : MonoBehaviour
         float y = GameObject.Find("Ship").transform.position.y;
 
         GameObject.Find("Ship").transform.position = new Vector3(x, y-10);
+
+        navModel.SetPositionY(y);
     }
 
     public void Left()
@@ -36,14 +43,19 @@ public class NavController : MonoBehaviour
         float y = GameObject.Find("Ship").transform.position.y;
 
         GameObject.Find("Ship").transform.position = new Vector3(x-10, y);
+
+        navModel.SetPositionX(x);
     }
 
     public void Right()
     {
         float x = GameObject.Find("Ship").transform.position.x;
         float y = GameObject.Find("Ship").transform.position.y;
+        x += 10;
 
-        GameObject.Find("Ship").transform.position = new Vector3(x+10, y);
+        GameObject.Find("Ship").transform.position = new Vector3(x, y);
+
+        navModel.SetPositionX(x);
     }
 
 }
